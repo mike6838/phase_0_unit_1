4fadf668efc517c277ddd97360fabdf97eefff3d
Michael Teevan <michael.teevan@gmail.com>
2014-07-27 17:05:27 -0700

    w3r6 - group proj - Person 1
    finished story

---
+++ week-3/6-group-project/gc1_user_stories.js
@@ -0,0 +1,19 @@
+// My role in the group is: Person 1
+
+// Here is my part of the challenge:
+
+// Hi,
+
+// I am a high school teacher that needs a program to calculate the grades
+// for both of the classes I teach. The only discernable difference between 
+// the two classes is that one has an even amount of students, while the 
+// other has an odd amount of students. I want to be able to calculate 
+// the total of my students’ grades for both classes. I also want the program 
+// to calculate the average of the students’ grades for both classes. My last 
+// request for you is that I’d like the program to find the median grade for 
+// each of my classes. Is that possible? I’d really appreciate it! 
+// Thank you so much!
+
+// Sincerely, 
+
+// Your favorite teacher, XOXO
