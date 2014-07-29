3a672b1bd6555edf7a1644a0a88da7235c22abcb
Michael Teevan <michael.teevan@gmail.com>
2014-07-29 14:56:34 -0700

    Completed w3r3

--- week-3/3-js-variables-objects/my_solution.js
+++ week-3/3-js-variables-objects/my_solution.js
@@ -1,24 +1,28 @@
-// I paired [by myself, with:] on this challenge.
+// I paired [with Rob Kramer] on this challenge.
 
 // __________________________________________
 // Write your code below.
 
-
-
-
-
-
-
+var secretNumber = 7
+var password = "just open the door"
+var allowedIn = true
+var members = ["John", "Mike", "Sally", "Mary"]
 
 // __________________________________________
 // Reflection: Use the reflection guidelines to write a reflection here. 
-// 
-// 
-// 
-// 
-// 
-// 
 
+// I paired with another on this exercise, which worked out well. I had 
+// a lot of trouble understanding the instructions on read.me file and 
+// my partner helped me better understand. The exercise was fairly easy, 
+// since there was no internal logic used creating functions. We were tasked 
+// to define variables and arrays, which we did. Following the directions 
+// for the pre written tests went pretty well, also. I’m use to how RSpec 
+// prints test results, going through all the failed tests, and not just 
+// stopping after the first one on the list had failed. Although, this might 
+// have been an option I overlooked on the consoles of Firebug and DevTools. 
+// I feel like I understand the syntax to create variables within JavaScript. 
+// The most tedious part of this challenge was understanding the instructions. 
+// I really appreciated the JS Intro guide that was linked on GitHub. 
 
 // __________________________________________
 // Test Code:  Do not alter code below this line.
