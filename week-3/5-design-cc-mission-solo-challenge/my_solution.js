826f0f1dcc25ca9ce97ed45598323aa8f9faaa73
Michael Teevan <michael.teevan@gmail.com>
2014-08-03 14:46:28 -0700

    solo challenge (incomplete)

--- week-3/5-design-cc-mission-solo-challenge/my_solution.js
+++ week-3/5-design-cc-mission-solo-challenge/my_solution.js
@@ -1,23 +1,75 @@
 // This is a solo challenge
 
 // Your mission description:
-// 
-// 
+// +++Alas, Coffee!+++
+// A group of friends, Nelly, Rob, Nate, and Michael, known around town as 'The Cool Kids' are 
+// looking for a place for them to get coffee and hangout. On their quest to find coffee and 
+// relaxation, a number of 'haters' make attempts to dissuade them from achieving their goal.
 // 
 
 // Pseudocode
-// 
-// 
-// 
-// 
-// 
+// Nelly variable
+// Rob variable
+// Nate variable
+// Michael Variable
+// haterOne variable
+// haterTwo variable
+// coffee shop variable
+// barista variable
 
 // Initial Code
 
+var nelly = {
+    name: "Nelly Lin",
+    health: 10,
+    team: "The Cool Kids"
+    weapon: "Smiles"
+};
 
+var rob = {
+    name: "Rob Kramer",
+    health: 10,
+    team: "The Cool Kids"
+    weapon: "Compliments"
+};
 
+var nate = {
+    name: "Nate Beers",
+    health: 10,
+    team: "The Cool Kids"
+    weapon: "Jokes"
+};
 
+var michael = {
+    name: "Michael Teevan",
+    health: 10,
+    team: "The Cool Kids"
+    weapon: "Credit Card"
+};
 
+var haterOne = {
+    name: "Hater McHaterson",
+    health: 10,
+    team: "haters"
+    weapon: "Critical Statements"
+};
+
+var haterTwo = {
+    name: "Hater McNegativity",
+    health: 10,
+    team: "haters"
+    weapon: "Negative Statements"
+};
+
+var barista = {
+	name: "The Barista"
+	health: 10;
+	weapon: "Coffee"
+};
+
+var coffeeShop = {
+	name: "The Coffee Shop"
+};
 
 // Refactored Code
 
