e731572c4c69cd7adfb4c4ca0f4b24800dbc99a5
Michael Teevan <michael.teevan@gmail.com>
2014-08-03 21:03:17 -0700

    Completed - w3r6

--- week-3/6-group-project/my_solution.js
+++ week-3/6-group-project/my_solution.js
@@ -1,3 +1,32 @@
-// My role in the group is:
+// My role in the group is: Person 1 -  Translate tests into user stories - Michael Teevan
 
 // Here is my part of the challenge:
+
+// Hi,
+
+// I am a high school teacher that needs a program to calculate the grades
+// for both of the classes I teach. The only discernable difference between 
+// the two classes is that one has an even amount of students, while the 
+// other has an odd amount of students. I want to be able to calculate 
+// the total of my students’ grades for both classes. I also want the program 
+// to calculate the average of the students’ grades for both classes. My last 
+// request for you is that I’d like the program to find the median grade for 
+// each of my classes. Is that possible? I’d really appreciate it! 
+// Thank you so much!
+
+// Sincerely, 
+
+// Your favorite teacher, XOXO
+
+
+// Reflection:
+
+// My experience working as a group on a project was a lot of fun. Although my group 
+// did not pass any of the tests due to my client story, I think we learned a lot about 
+// communication. I learned that in the user stories, I need to be more mindful of the 
+// variables used in testing and the objects that are used within the test, such as arrays. 
+// The problems my group faced was reading my user stories. The rest of the group created a 
+// working program using JavaScript based on my story, but unfortunately, it did not pass any of 
+// the tests because I failed in leading them. I feel confidence in most of the learning competencies. 
+// The part that I enjoyed the most was when it was all-complete was getting everyone together on Google 
+// Hangout and talking over what went wrong. 
